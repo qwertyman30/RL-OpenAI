9aa0a954da5944d42c89cba3ad7fbdd797839db5
unknown <monish.789@gmail.com>
2020-06-23 06:51:47 +0200

    Update network and cartpole test

--- reinforcement_learning/agent/networks.py
+++ reinforcement_learning/agent/networks.py
@@ -18,3 +18,37 @@ class MLP(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         return self.fc3(x)
+        
+
+class CNN(nn.Module):
+
+    def __init__(self, history_length=0, n_classes=3): 
+        super().__init__()
+        # TODO : define layers of a convolutional neural network
+        self.conv1 = nn.Conv2d(1+history_length, 6, 3)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(7056, 1024)
+        self.fc2 = nn.Linear(1024, 128)
+        self.fc3 = nn.Linear(128, 32)
+        self.batch_norm = nn.BatchNorm1d(32)
+        self.fc4 = nn.Linear(32, n_classes)
+
+    def forward(self, x):
+        # TODO: compute forward pass
+        x = self.conv1(x)
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(x)
+
+        shape = x.shape
+        x = x.view(-1, shape[1] * shape[2] * shape[3])
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, p=0.2)
+        x = F.relu(self.fc2(x))
+        x = F.dropout(x, p=0.2)
+        x = F.relu(self.fc3(x))
+        x = self.batch_norm(x)
+        x = F.relu(self.fc4(x))
+        return F.log_softmax(x, dim=1)
--- reinforcement_learning/test_cartpole.py
+++ reinforcement_learning/test_cartpole.py
@@ -18,9 +18,9 @@ if __name__ == "__main__":
     state_dim = 4
     num_actions = 2
     
-    Q = MLP(state_dim, num_actions, hidden_dim=50)
-    Q_target = MLP(state_dim, num_actions, hidden_dim=50)
-    agent = DQNAgent(Q, Q_target, num_actions, gamma=0.9, batch_size=20, epsilon=0.1, tau=0.01, lr=0.001, history_length=0)
+    Q = MLP(state_dim, num_actions)
+    Q_target = MLP(state_dim, num_actions)
+    agent = DQNAgent(Q, Q_target, num_actions)
     agent.load("C:\\Users\\Monish\\Desktop\\workspace\\exercise3_R\\reinforcement_learning\\models_cartpole\\dqn_agent_149.pt")
  
     n_test_episodes = 15
